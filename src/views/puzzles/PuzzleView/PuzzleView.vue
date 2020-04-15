1bf7ee6db1693de668230bc6acea16e50da5c667
Abhi Garg <agarg5@alumni.stanford.edu>
2020-04-15 14:36:15 +0200

    puzzle view styling progress

--- src/views/puzzles/PuzzleView/PuzzleView.vue
+++ src/views/puzzles/PuzzleView/PuzzleView.vue
@@ -1,6 +1,51 @@
 <template>
   <EternaPage v-if="pageData" :title="pageData.puzzle.title">
-    <div class="page-content" v-html="pageData.puzzle.body"></div>
+    <div class="page-content">
+      <div class=" d-flex">
+        <div>
+          <h2>About the Puzzle</h2>
+          <p>Welcome to the easiest puzzle of the SRP RNA series!</p>
+          <h3>THE SCIENCE</h3>
+          <p>
+            The SRP RNA, also known as 7SL, 6S, or 4.5S RNA, is the RNA component of the signal
+            recognition particle (SRP) ribonucleoprotein complex. SRP is a universally conserved
+            ribonucleoprotein that directs the traffic of proteins within the cell and allows them
+            to be secreted.
+          </p>
+          <h3>YOUR MISSION</h3>
+          <p>
+            Design the SRP RNA with following specifications: • Minimum 5 GU Pairs
+          </p>
+          <h3>MORE INFORMATION</h3>
+          <p>For more information please visit: Crystal structure Image and Protein Data Bank</p>
+        </div>
+
+        <div>
+          <div
+            :style="{ width: '334px', height: '365px', backgroundColor: '#041227', opacity: '0.8' }"
+          ></div>
+          <b-button type="submit" variant="primary" class="submit-button">
+            Play Now
+          </b-button>
+        </div>
+      </div>
+
+      <hr class="top-border" />
+
+      <h2>Quests</h2>
+      <p>This puzzle appears in the following quests:</p>
+      <div>
+        <img
+          src="https://cdn.zeplin.io/5e88563a3843011f95808b2f/assets/5ED5D090-6F62-4DF8-8C54-CC71306A4B16.png"
+        />
+        <img
+          src="https://cdn.zeplin.io/5e88563a3843011f95808b2f/assets/6A70A1E1-9A81-4BA0-B765-A12B8F821300.png"
+        />
+        <img
+          src="https://cdn.zeplin.io/5e88563a3843011f95808b2f/assets/E280848F-6347-4CC5-A215-F08B1F55ED1B.png"
+        />
+      </div>
+    </div>
 
     <template #sidebar="{ isInSidebar }">
       <TagsPanel :tags="['#SRP', '#easy']" :isInSidebar="isInSidebar" />

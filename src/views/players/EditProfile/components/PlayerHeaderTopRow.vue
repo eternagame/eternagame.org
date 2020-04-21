013467231a21ffb74aa1230477910863029ca14d
Abhishek Garg <agarg5@alumni.stanford.edu>
2020-04-21 12:22:08 +0200

    Revert "fixed styles in Edit Profile page"

--- src/views/players/EditProfile/components/PlayerHeaderTopRow.vue
+++ src/views/players/EditProfile/components/PlayerHeaderTopRow.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex justify-content-between">
-    <b-button type="secondary">{{
+    <b-button type="secondary" style="margin-left:10px">{{
       $t('edit-profile:replace-image')
     }}</b-button>
     <b-button type="submit" style="margin-left:10px" variant="primary" @click="submit">{{

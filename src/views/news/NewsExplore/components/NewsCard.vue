e6c1f387a7ee37d79248e242325a26d554c70638
Austin Chen <akrolsmir@gmail.com>
2020-05-08 00:25:16 -0700

    Style each card in news listing
    Clubhouse 538

--- src/views/news/NewsExplore/components/NewsCard.vue
+++ src/views/news/NewsExplore/components/NewsCard.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="page-content card" @click="redirect()">
-    <!-- Padding: top right bottom  left -->
-    <div class="container" style="padding:10px">
+    <div class="container">
       <div class="row justify-content-between">
-        <div class="col">
-          <div :style="{ color: categoryColor }">{{ `${category.toUpperCase()}` }}</div>
+        <div class="col p-0">
+          <!-- Note: Any space between these tags will misalign the Type tag. -->
+          <div class="b" :style="{ color: typeColor }">{{ formattedType }}</div>
         </div>
-        <div class="col" style="text-align:right">
-          <div style="opacity: 0.5;">{{ timeStamp }}</div>
+        <div class="col p-0" style="text-align:right">
+          <div class="b" style="opacity: 0.5;">{{ created }}</div>
         </div>
       </div>
       <div class="row">
-        <p style="font-size:20px;font-weight:bold;marginBottom:0px">{{ title }}</p>
-        <div v-dompurify-html="body" class="text" />
+        <h3 class="card-title">{{ title }}</h3>
+        <div v-dompurify-html="strippedBody" class="text" />
       </div>
     </div>
   </div>
@@ -26,7 +26,7 @@
     components: {},
   })
   export default class NewsCard extends Vue {
-    @Prop() private timeStamp!: string;
+    @Prop() private created!: string;
 
     @Prop() private title!: string;
 
@@ -34,19 +34,34 @@
 
     @Prop() private nid!: string;
 
-    @Prop({ default: '' }) private category!: string;
+    @Prop({ default: 'blogs' }) private type!: string;
 
     redirect() {
       this.$router.push(`/news/${this.nid}`);
     }
 
-    get categoryColor() {
-      switch (this.category.toLowerCase()) {
+    get formattedType(): string {
+      const formatted = this.type.toUpperCase();
+      if (formatted === 'BLOGS') {
+        // Unpluralize, since it sounds better
+        return 'BLOG';
+      }
+      return formatted;
+    }
+
+    get strippedBody(): string {
+      // For now, remove all html tags, since <ul> and <img> can break formatting.
+      return this.body.replace(/(<([^>]+)>)/gi, '');
+    }
+
+    get typeColor() {
+      switch (this.type.toLowerCase()) {
         case 'blogs':
           return '#53b64e';
         case 'labs':
           return '#50b2dc';
         case 'announcements':
+        case 'news':
           return '#f39c12';
         default:
           return '#53b64e';
@@ -66,9 +81,26 @@
     padding: 11.25px !important;
   }
 
+  .card-title {
+    font-size: 1.3rem;
+    font-weight: bold;
+    margin: 0.4rem 0;
+  }
+
+  .b {
+    font-weight: bold;
+  }
+
   .card {
-    margin-bottom: 50px;
+    padding: 1rem 2rem;
+    margin-bottom: 1.5rem;
     max-height: 600px;
+    cursor: pointer;
+    transition: background-color 0.5s ease;
+  }
+
+  .card:hover {
+    background-color: #21508c;
   }
 
   .text {
@@ -80,7 +112,7 @@
     overflow: hidden;
     text-overflow: ellipsis;
     display: -webkit-box;
-    -webkit-line-clamp: 10; /* number of lines to show */
+    -webkit-line-clamp: 4; /* number of lines to show */
     -webkit-box-orient: vertical;
   }
 </style>

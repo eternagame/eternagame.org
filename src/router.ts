5148a4e314054882ca4319b1a48a0c47740e06cf
Austin Chen <akrolsmir@gmail.com>
2020-05-11 03:15:25 -0700

    Only authenticate once each pageload

--- src/router.ts
+++ src/router.ts
@@ -151,7 +151,9 @@ export default function createRouter() {
 
   router.beforeEach(async (to: Route, from: Route, next: RouteCallback<any>) => {
     const userStore = router.app.$vxm.user;
-    if (window.localStorage.getItem('loggedIn') === 'true') await userStore.authenticate();
+    if (window.localStorage.getItem('loggedIn') === 'true' && !userStore.triedAuthenticating) {
+      await userStore.authenticate();
+    }
     if (!to.matched.length) {
       next('/lost');
     }

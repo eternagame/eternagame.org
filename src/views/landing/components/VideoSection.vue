7b00cf5c500e17b23f4f6f8f077e557ef7fcb7eb
Austin Chen <akrolsmir@gmail.com>
2020-05-13 12:54:44 -0700

    Hardcode 'Play Now' link on landing page
    Fixes #47. To links don't work for addresses not in router.ts.
    
    Hardcoding to https://eternagame so that it is testable on local, but eventually we want to make all https://eternagame links be relative

--- src/views/landing/components/VideoSection.vue
+++ src/views/landing/components/VideoSection.vue
@@ -19,7 +19,7 @@
               <h4 class="text-block">
                 {{ $t('video-section:sub-heading') }}
               </h4>
-              <b-button variant="primary" size="lg" to="/game/puzzle/6502927/" class="button">{{
+              <b-button variant="primary" size="lg" href="https://eternagame.org/game/puzzle/6502927/" class="button">{{
                 $t('video-section:main-action')
               }}</b-button>
               <b-button variant="secondary" size="lg" to="/about" class="ml-3 button">

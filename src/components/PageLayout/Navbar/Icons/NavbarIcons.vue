00a6ca0f58ac1830481d3d7c85327ababb774dee
AlexHalb <ahalbesleben@holyspirit-al.com>
2020-05-14 15:55:22 -0500

    Reverted some changes

--- src/components/PageLayout/Navbar/Icons/NavbarIcons.vue
+++ src/components/PageLayout/Navbar/Icons/NavbarIcons.vue
@@ -2,7 +2,7 @@
   <div>
     <UserIcon class="float-lg-right icon" />
     <BellIcon class="float-lg-right icon" />
-    <ChatIcon class="float-lg-right icon" :isInSideBar="isInSideBar"  style="float:left;"/>
+    <ChatIcon class="float-lg-right icon" :isInSideBar="isInSideBar"/>
   </div>
 </template>
 <script lang="ts">

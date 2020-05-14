a76404689765af965f78d6decc4ebade85e750db
Jonathan Romano <jonathan@luxaritas.com>
2020-05-13 23:55:50 -0400

    Fix lab browse link

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const PUZZLE_ROUTE_PREFIX = `${process.env.VUE_APP_API_BASE_URL}/game/puzzle/`;
 
-export const PUZZLE_ROUTE_BROWSE_PREFIX = `${process.env.VUE_APP_API_BASE_URL}/browse/puzzle/`;
+export const PUZZLE_ROUTE_BROWSE_PREFIX = `${process.env.VUE_APP_API_BASE_URL}/game/browse/`;
 
 export const DEFAULT_PLAYER_PICTURE = 'https://s3.amazonaws.com/eterna/icon_img/question.png';
 

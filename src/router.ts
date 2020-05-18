3361f7f198bb6eb7ee6ed4b307f94b4844d728a5
Austin Chen <akrolsmir@gmail.com>
2020-05-18 14:00:20 -0700

    Routes direct to top of page
    Fixes #118

--- src/router.ts
+++ src/router.ts
@@ -117,6 +117,14 @@ export default function createRouter() {
         component: () => import('./views/chat/FullPageChat.vue'),
       },
     ],
+    scrollBehavior(to, from, savedPosition) {
+      // Navigate to previous scroll position, or else top of page
+      // https://router.vuejs.org/guide/advanced/scroll-behavior.html
+      if (savedPosition) {
+        return savedPosition;
+      }
+      return { x: 0, y: 0 };
+    },
   });
 
   router.beforeEach(async (to: Route, from: Route, next: RouteCallback<any>) => {

90de06516ed7e4a86c1d0274587a2deb4c146a5e
Tamas Kalman <ktamas77@gmail.com>
2020-05-27 16:55:21 -0700

    Fixing Edit Player URL

--- src/views/players/EditProfile/components/EditPlayerHeaderImage.vue
+++ src/views/players/EditProfile/components/EditPlayerHeaderImage.vue
@@ -22,6 +22,7 @@
 
 <script lang="ts">
   import { Component, Vue, Mixins, Prop } from 'vue-property-decorator';
+  import Utils from '@/utils/utils';
   import { UserData } from '@/types/common-types';
   import { DEFAULT_PLAYER_PICTURE } from '@/utils/constants';
 
@@ -34,7 +35,7 @@
     }
 
     get picture() {
-      return this.user.picture && `${process.env.VUE_APP_API_BASE_URL}/${this.user.picture}`;
+      return Utils.getAvatar(this.user.picture);
     }
   }
 </script>

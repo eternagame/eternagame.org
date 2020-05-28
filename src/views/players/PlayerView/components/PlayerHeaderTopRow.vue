df9ab03821092e00553d316faff009f23c97d630
Abhi Garg <agarg5@alumni.stanford.edu>
2020-05-28 16:28:29 +0200

    allow player to follow other players

--- src/views/players/PlayerView/components/PlayerHeaderTopRow.vue
+++ src/views/players/PlayerView/components/PlayerHeaderTopRow.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="d-flex">
-    <!--b-form-checkbox
+    <b-form-checkbox
       class="mt-1 mr-3"
       id="checkbox-1"
       name="checkbox-1"
-      value="accepted"
-      unchecked-value="not_accepted"
+      @change="toggleFollow"
+      v-model="follows"
       >{{ $t('player-view:following') }}
-    </b-form-checkbox-->
+    </b-form-checkbox>
 
     <b-button
       type="submit"
-      style="margin-left:10px; padding: 0 30px;"
+      style="margin-left:10px; padding: 5px 30px;"
       variant="primary"
       @click="messageUser"
       >{{ $t('player-view:message') }}</b-button
@@ -25,20 +25,28 @@
   import { UserData } from '@/types/common-types';
   import SmartLink from '@/components/Common/SmartLink.vue';
 
+  const UNFOLLOW_ROUTE = `/post/?type=expire_follow&follow_type=user&id=`;
+
+  const FOLLOW_ROUTE = `/post/?type=follow&follow_type=user&id=`;
+
   @Component({ components: { SmartLink } })
   export default class PlayerHeaderTopRow extends Vue {
     @Prop() user!: UserData;
 
-    private follows = false;
+    @Prop()
+    private followList!: UserData[];
+
+    private follows: boolean = this.followList
+      .map((u: UserData) => u.uid)
+      .includes(String(this.$vxm.user.uid));
+
+    toggleFollow() {
+      const route = this.follows ? FOLLOW_ROUTE : UNFOLLOW_ROUTE;
+      axios.post(route + this.$vxm.user.uid);
+    }
 
     messageUser() {
       this.$router.push(`/feed?message=${this.user.name}`);
     }
-
-  /* created() {
-      axios.get(`/get/?type=follow&id=${'2804'}`).then(response => {
-        this.follows = response.data.data.follows.includes(this.user.uid);
-      });
-    } */
   }
 </script>

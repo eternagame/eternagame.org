05398d9d6c5cd4ee6a06f28d2d36a27ecabebeea
Jonathan Romano <jonathan@luxaritas.com>
2020-08-02 02:35:12 -0400

    Fix cert path

--- src/views/players/PlayerView/components/PlayerHeaderIcons.vue
+++ src/views/players/PlayerView/components/PlayerHeaderIcons.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="d-flex"><router-link
-      :to="`../certificate/${user.uid}`"
+  <div class="d-flex">
+    <a
+      :href="`${BASE_URL_PREFIX}/certificate/${user.uid}`"
       v-if="isCurrentUser"
     >
       Certificate
-    </router-link>
+    </a>
     <img src="@/assets/dollar.svg" class="icon ml-4" /> {{ user.points }}
     <img src="@/assets/test-tube.svg" class="icon ml-4" /> {{ user.synthesized_count }}
     <img src="@/assets/calendar.svg" class="icon ml-4" /> {{ formattedCreated }}
@@ -27,6 +28,8 @@
     get isCurrentUser() {
       return this.$vxm.user.uid && this.$vxm.user.uid.toString() === this.user.uid;
     }
+
+    private BASE_URL_PREFIX: string = process.env.VUE_APP_API_BASE_URL;
   }
 </script>
 

01162194007a097c04d8648a2317032d2be9be85
Jonathan Romano <jonathan@luxaritas.com>
2020-08-05 15:04:42 -0400

    Nicer page not found - thanks Nessa!

--- src/views/lost/NotFound.vue
+++ src/views/lost/NotFound.vue
@@ -1,7 +1,15 @@
 <template>
   <EternaPage title="Not Found">
     <div class="page">
-      Could not find this page sorry, please go back
+      <h1>Not Found</h1>
+      <div class="text-muted subtitle">
+        Sorry, we couldn't find that.
+        <router-link to="/">Take me home!</router-link>
+      </div>
+      <figure class="text-left mx-auto mt-4 d-inline-block figure">
+        <img src="~@/assets/watblob.png" class="figure-img">
+        <figcaption class="figure-caption mt-n4">Image by Ness</figcaption>
+      </figure>
     </div>
   </EternaPage>
 </template>
@@ -16,8 +24,15 @@
 </script>
 
 <style lang="scss" scoped>
+  @import '@/styles/global.scss';
+
   .page {
     margin-top: 120px;
     padding: 20px;
+    text-align: center;
+  }
+
+  .subtitle {
+    font-size: 2rem;
   }
 </style>

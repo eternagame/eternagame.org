b33733eec7677257e48abec9411b7e9f4ef9468e
Afika Nyati <afika.a.nyati@gmail.com>
2021-04-30 22:14:42 -0700

    update history page layout

--- src/views/history/History.vue
+++ src/views/history/History.vue
@@ -1,115 +1,130 @@
 <template>
-  <EternaPage>
-    <!-- Note: Header date is copied from Terms & Conditions, so may not be correct. -->
-    <div class="history-container">
-      <div class="p-2">
-        <h4 class="header">
-          {{ $t('history:origins-header') }}
-        </h4>
-        <p class="description">
-          {{ $t('history:origins-body-1') }}
-          {{ $t('history:origins-body-2') }}
-        </p>
-        <section class="history-origins-video">
+  <EternaPage title="History of Eterna">
+    <div class="origin-container" :id="sections[0].href">
+      <h4 class="header">
+        {{ $t('history:origins-header') }}
+      </h4>
+      <div class="history-paragraph">
+        <section class="history-media-asset">
           <iframe
             src="https://www.youtube.com/embed/bTlNNFQxs_A"
             frameborder="0"
             allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
           ></iframe>
+          <p class="history-media-asset-description">
+            Eterna's founders were featured in this video from PBS NOVA about innovations in citizen science.
+          </p>
         </section>
-        <br />
-        <h4 class="header">
-          {{ $t('history:science-header') }}
-        </h4>
-        <p class="description">
-          {{ $t('history:science-body-1') }}
-          <a href="https://en.wikipedia.org/wiki/Nucleic_acid_structure_determination#SHAPE" target="_blank">{{ $t('history:science-body-2') }}</a>
-          {{ $t('history:science-body-3') }}
-          <a href="https://en.wikipedia.org/wiki/Flavin_mononucleotide" target="_blank">{{ $t('history:science-body-4') }}</a>
-          {{ $t('history:science-body-5') }}
-          <a href="https://www.pnas.org/content/111/6/2122" target="_blank">{{ $t('history:science-body-6') }}</a>
-          {{ $t('history:science-body-7') }}
-        </p>
-        <p class="description">
-          {{ $t('history:science-body-8') }}
-          <a href="https://www.sciencedirect.com/science/article/pii/S0022283615006567?via%3Dihub" target="_blank">{{ $t('history:science-body-9') }}</a>
-          {{ $t('history:science-body-10') }}
-          <a href="https://www.biorxiv.org/content/10.1101/281147v2" target="_blank">{{ $t('history:science-body-11') }}</a>
-        </p>
-        <p class="description">
-          {{ $t('history:science-body-12') }}
-          <a href="https://en.wikipedia.org/wiki/MS2_tagging" target="_blank">{{ $t('history:science-body-13') }}</a>
-          {{ $t('history:science-body-14') }}
-        </p>
-        <section class="history-origins-video">
-          <iframe
-            src="https://www.youtube.com/embed/V2qtNyJ3KQg"
-            frameborder="0"
-            allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
-        </section>
-        <p class="description">
-          {{ $t('history:science-body-15') }}
-        </p>
-        <p class="description">
-          {{ $t('history:science-body-16') }}
-        </p>
-        <br />
-        <div v-if="data" class="stats-container">
-          <div class="stat">
-            <h2 class="stat-number">
-              {{ formattedNumber(data.num_all_publications) }}
-            </h2>
-            <h3 class="stat-description">
-              Total Publications
-            </h3>
-          </div>
-          <div class="stat">
-            <h2 class="stat-number">
-              {{ formattedNumber(data.num_contributors) }}
-            </h2>
-            <h3 class="stat-description">
-              Total Publication Contributors
-            </h3>
-          </div>
-          <div class="stat">
-            <h2 class="stat-number">
-              {{ formattedNumber(data.num_lab_solutions) }}
-            </h2>
-            <h3 class="stat-description">
-              Total Lab Solutions
-            </h3>
-          </div>
-          <div class="stat">
-            <h2 class="stat-number">
-              {{ formattedNumber(data.num_solved_puzzles) }}
-            </h2>
-            <h3 class="stat-description">
-              Total Puzzles Solved
-            </h3>
-          </div>
-        </div>
-        <br />
-        <h4 class="header">
-          {{ $t('history:software-header') }}
-        </h4>
-        <p class="description">
-          {{ $t('history:software-body-1') }}
-        </p>
-        <p class="description">
-          {{ $t('history:software-body-2') }}
-        </p>
-        <h4 class="header">
-          {{ $t('history:community-header') }}
-        </h4>
-        <p class="description">
-          {{ $t('history:community-body-1') }}
-        </p>
+        {{ $t('history:origins-body-1') }}
+        {{ $t('history:origins-body-2') }}
       </div>
-      <hr class="top-border" />
     </div>
+    <div v-if="data" class="stats-container">
+      <div class="stat">
+        <h2 class="stat-number">
+          {{ formattedNumber(data.num_lab_solutions) }}
+        </h2>
+        <h3 class="stat-description">
+          Lab Solutions
+        </h3>
+      </div>
+      <div class="stat">
+        <h2 class="stat-number">
+          {{ formattedNumber(data.num_solved_puzzles) }}
+        </h2>
+        <h3 class="stat-description">
+          Puzzles Solved
+        </h3>
+      </div>
+      <div class="stat">
+        <h2 class="stat-number">
+          {{ formattedNumber(data.num_all_publications) }}
+        </h2>
+        <h3 class="stat-description">
+          Publications
+        </h3>
+      </div>
+      <div class="stat">
+        <h2 class="stat-number">
+          {{ formattedNumber(data.num_contributors) }}
+        </h2>
+        <h3 class="stat-description">
+          Publication Contributors
+        </h3>
+      </div>
+    </div>
+    <div class="science-container" :id="sections[1].href">
+      <h4 class="header">
+        {{ $t('history:science-header') }}
+      </h4>
+      <div class="history-paragraph">
+        <section class="history-media-asset">
+          <img src="@/assets/history/science.png" />
+          <p class="history-media-asset-description">
+            Eterna designs are tested experimentally and scored by reactivity, then compared to algorithmically designed structures.
+          </p>
+        </section>
+        {{ $t('history:science-body-1') }}
+        <a href="https://en.wikipedia.org/wiki/Nucleic_acid_structure_determination#SHAPE" target="_blank">{{ $t('history:science-body-2') }}</a>
+        {{ $t('history:science-body-3') }}
+        <a href="https://en.wikipedia.org/wiki/Flavin_mononucleotide" target="_blank">{{ $t('history:science-body-4') }}</a>
+        {{ $t('history:science-body-5') }}
+        <a href="https://www.pnas.org/content/111/6/2122" target="_blank">{{ $t('history:science-body-6') }}</a>
+        {{ $t('history:science-body-7') }}
+      </div>
+      <div class="history-paragraph">
+        {{ $t('history:science-body-8') }}
+        <a href="https://www.sciencedirect.com/science/article/pii/S0022283615006567?via%3Dihub" target="_blank">{{ $t('history:science-body-9') }}</a>
+        {{ $t('history:science-body-10') }}
+        <a href="https://www.biorxiv.org/content/10.1101/281147v2" target="_blank">{{ $t('history:science-body-11') }}</a>
+      </div>
+      <div class="history-paragraph">
+        {{ $t('history:science-body-12') }}
+        <a href="https://en.wikipedia.org/wiki/MS2_tagging" target="_blank">{{ $t('history:science-body-13') }}</a>
+        {{ $t('history:science-body-14') }}
+      </div>
+      <div class="history-paragraph">
+        {{ $t('history:science-body-15') }}
+      </div>
+      <div class="history-paragraph">
+        {{ $t('history:science-body-16') }}
+      </div>
+    </div>
+    <div class="software-container" :id="sections[2].href">
+      <h4 class="header">
+        {{ $t('history:software-header') }}
+      </h4>
+      <div class="history-paragraph">
+        <section class="history-media-asset">
+          <img src="@/assets/history/software.png" />
+          <p class="history-media-asset-description">
+            Eterna designs are tested experimentally and scored by reactivity, then compared to algorithmically designed structures.
+          </p>
+        </section>
+        {{ $t('history:software-body-1') }}
+      </div>
+      <div class="history-paragraph">
+        {{ $t('history:software-body-2') }}
+      </div>
+    </div>
+    <div class="community-container" :id="sections[3].href">
+      <h4 class="header">
+        {{ $t('history:community-header') }}
+      </h4>
+      <div class="history-paragraph">
+        <section class="history-media-asset">
+          <img src="@/assets/history/community.png" />
+          <p class="history-media-asset-description">
+            Eterna players, developers, and researchers gather at Stanford University for Eternacon, an annual player-organized conference.
+          </p>
+        </section>
+        {{ $t('history:community-body-1') }}
+      </div>
+    </div>
+    <template #sidebar="{ isInSidebar }">
+      <HistoryInfoPanel :sections="sections" :isInSidebar="isInSidebar" />
+    </template>
   </EternaPage>
 </template>
 
@@ -118,21 +133,47 @@
   import EternaPage from '@/components/PageLayout/EternaPage.vue';
   import DropdownSidebarPanel, { Option } from '@/components/Sidebar/DropdownSidebarPanel.vue';
   import FetchMixin from '@/mixins/FetchMixin';
+  import HistoryInfoPanel from './components/HistoryInfoPanel.vue';
   import HistoryData from './types';
   
 
   @Component({
-    components: { EternaPage, DropdownSidebarPanel },
+    components: {
+      EternaPage,
+      DropdownSidebarPanel,
+      HistoryInfoPanel
+    },
   })
   export default class History extends Mixins(FetchMixin) {
     data: HistoryData | null = null;
+
+    sections: object[] = [
+      {
+        id: 0,
+        title: "Origin",
+        href: "origin"
+      },
+      {
+        id: 1,
+        title: "Science",
+        href: "science"
+      },
+      {
+        id: 2,
+        title: "Software",
+        href: "software"
+      },
+      {
+        id: 3,
+        title: "Community",
+        href: "community"
+      }
+    ];
 
     async fetch() {
       const historyResults = (
         await this.$http.get(`/get/?type=history`)
       ).data.data.history as HistoryData;
-      console.log("HISTORY: ", historyResults);
-      
       
       this.data = historyResults;
     }
@@ -145,46 +186,89 @@
 <style lang="scss" scoped>
   @import '@/styles/global.scss';
 
-  .history-container {
+  .origin-container {
     position: relative;
-    left: 50%;
-    display: inline-block;
     border-radius: 5px;
     background-color: #043468;
-    padding: 16px;
-    transform: translateX(-50%);
+    padding: 25px;
+    margin-bottom: 30px;
   }
 
-  .history-origins-video {
-    display: flex;
-    justify-content: center;
-    margin-bottom: 50px;
+  .science-container {
+    position: relative;
+    padding-bottom: 25px;
+    margin-bottom: 30px;
+    border-bottom: 1px solid #2F94D1;
+  }
+
+  .software-container {
+    position: relative;
+    padding-bottom: 25px;
+    margin-bottom: 30px;
+    border-bottom: 1px solid #2F94D1;
+  }
+
+  .community-container {
+    position: relative;
+    margin-bottom: 30px;
+  }
+
+  .header {
+    margin-bottom: 1.5rem;
+  }
+
+  .history-media-asset {
+    float: right;
+    margin-left: 15px;
+    width: 400px;
 
     & iframe {
       width: 400px;
       height: 250px;
       border-radius: 5px;
     }
+
+    & img {
+      width: 400px;
+      border-radius: 5px;
+      margin-bottom: 10px;
+    }
+  }
+
+  .history-media-asset-description {
+    display: inline-block;
+    font-size: 0.7rem;
+  }
+  
+  .history-paragraph {
+    margin-bottom: 20px;
   }
 
   .stats-container {
-    display: flex;
-    flex-direction: row;
     width: 100%;
-
-    @include media-breakpoint-down(md) {
-      display: block;
-    }
+    padding-bottom: 25px;
+    margin-bottom: 25px;
+    border-bottom: 1px solid #2F94D1;
 
     .stat {
-      display: flex;
+      display: inline-flex;
       flex-direction: column;
-      flex: 0.25;
-      background-color: #05224b;
-      margin: 10px;
-      padding: 10px;
+      width: 50%;
+      background-color: #043468;
+      margin-bottom: 10px;
+      
+      padding: 15px;
       border-radius: 5px;
-      box-shadow: 1px 1px 0px 0px rgb(94 93 102 / 8%), 2px 2px 3px 0px rgb(94 93 102 / 10%);
+
+      &:nth-child(2n+1) {
+        margin-right: 10px;
+        width: calc(50% - 10px);
+
+        @include media-breakpoint-down(sm) {
+          margin-right: 0px;
+          width: 50%;
+        }
+      }
     
       .stat-number {
         font-size: 2rem;
@@ -193,25 +277,11 @@
       }
 
       .stat-description {
-        max-width: 20ch;
         font-size: 0.8rem;
-        text-transform: uppercase;
-        color: #fac244;
+        color: #2F94D1;
         text-align: center;
         margin: 0 auto;
       }
     }
-  }
-
-  .header {
-    margin-top: 2rem;
-    margin-bottom: 1.5rem;
-    text-align: center;
-  }
-
-  .description {
-    max-width: 70ch;
-    padding-bottom: 2rem;
-    margin: 0 auto;
   }
 </style>
---
+++ src/views/history/components/HistoryInfoPanel.vue
@@ -0,0 +1,49 @@
+<template>
+  <SidebarPanel
+    :isInSidebar="isInSidebar"
+    header="Sections"
+    headerIcon="@/assets/info.svg"
+  >
+    <ul class="page-sections-container">
+      <li class="page-section" v-for="section in sections" :key="`#${section.id}`">
+        <a :href="section.href">
+          {{ section.title }}
+        </a>
+      </li>
+    </ul>
+  </SidebarPanel>
+</template>
+
+<script lang="ts">
+  import { Component, Prop } from 'vue-property-decorator';
+  import { mixins } from 'vue-class-component';
+  import SidebarPanel from '@/components/Sidebar/SidebarPanel.vue';
+  import SidebarPanelMixin from '@/mixins/SidebarPanel';
+
+  @Component({
+    components: {
+      SidebarPanel,
+    },
+  })
+  export default class HistoryInfoPanel extends mixins(SidebarPanelMixin) {
+    @Prop({ required: true }) readonly sections!: object[];
+  }
+</script>
+
+<style scoped lang="scss">
+  .page-sections-container {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .page-section {
+    padding: 10px;
+    padding-left: 0px;
+    border-bottom: 1px solid #1E395D;
+
+    & a {
+      color: #B4BCC2;
+    }
+  }
+</style>

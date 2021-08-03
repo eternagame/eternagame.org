e89c0a45cc9310d6f3e3ab946528dcddc5145faa
Jonathan Romano <jonathan@luxaritas.com>
2021-08-02 23:26:33 -0400

    Restore account created date on profile

--- src/views/players/PlayerView/components/PlayerHeaderIcons.vue
+++ src/views/players/PlayerView/components/PlayerHeaderIcons.vue
@@ -8,7 +8,7 @@
     </a>
     <img src="@/assets/dollar.svg" class="icon ml-4" /> {{ user.points }}
     <img src="@/assets/test-tube.svg" class="icon ml-4" /> {{ user.synthesized_count }}
-    <img src="@/assets/calendar.svg" class="icon ml-4" /> {{ formattedCreated }}
+    <img src="@/assets/calendar.svg" class="icon ml-4" /> {{ user.created }}
   </div>
 </template>
 
@@ -19,11 +19,6 @@
   @Component({})
   export default class PlayerHeaderIcons extends Vue {
     @Prop({ required: true }) readonly user!: UserData;
-
-    get formattedCreated(): string {
-      const start = this.user.created.indexOf(' ');
-      return this.user.created.substring(start);
-    }
 
     get isCurrentUser() {
       return this.$vxm.user.uid && this.$vxm.user.uid.toString() === this.user.uid;

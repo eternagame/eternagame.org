c6897dce5194fc946e8bccc481c6997ecb3c69db
Jonathan Romano <jonathan@luxaritas.com>
2021-08-10 00:55:10 -0400

    Update contact and tweak formatting on code of conduct page

--- src/views/conduct/CodeOfConduct.vue
+++ src/views/conduct/CodeOfConduct.vue
@@ -4,38 +4,29 @@
     :header_title="$t('code-of-conduct:title')"
     :header_date="$t('terms:header-date')"
   >
-    <!-- Note: Header date is copied from Terms & Conditions, so may not be correct. -->
+    <!-- Note: If you make meaningful updates this page, make sure to update the header date -->
     <div class="page-content mt-2">
       <div class="p-2">
         <p>{{ $t('code-of-conduct:section1-text') }}</p>
-        <br />
         <ul style="padding-left: 20px;">
-          <li>
-            {{ $t('code-of-conduct:list-item-1') }}
-          </li>
+          <li>{{ $t('code-of-conduct:list-item-1') }}</li>
           <li>{{ $t('code-of-conduct:list-item-2') }}</li>
           <li>{{ $t('code-of-conduct:list-item-3') }}</li>
           <li>
             {{ $t('code-of-conduct:list-item-4') }}
-            <a href="http://en.wikipedia.org/wiki/Troll_(Internet)" target="_blank">{{
-              $t('code-of-conduct:list-item-4-troll')
-            }}</a>
+            <a href="http://en.wikipedia.org/wiki/Troll_(Internet)" target="_blank">
+              {{ $t('code-of-conduct:list-item-4-troll') }}
+            </a>
           </li>
         </ul>
-        <p style="padding-top: 15px;">
-          {{ $t('code-of-conduct:text1') }}
-        </p>
-        <br />
+        <p>{{ $t('code-of-conduct:text1') }}</p>
         <p>
           {{ $t('code-of-conduct:text2') }}
-          <a href="/web/player/48290/" target="_blank">{{ $t('code-of-conduct:text3') }}</a>
-          {{ $t('code-of-conduct:text4') }}
-          <br /><br />
-          <br /><br />
-          {{ $t('code-of-conduct:text5') }}
-          <br /><br />
-          {{ $t('code-of-conduct:text6') }}
+          <a href="mailto:support@eternagame.org">support@eternagame.org</a>
+          {{ $t('code-of-conduct:text3') }}
         </p>
+        <p>{{ $t('code-of-conduct:text4') }}</p>
+        <p>{{ $t('code-of-conduct:text5') }}</p>
       </div>
       <hr class="top-border" />
     </div>

60b774584ef078baf7e67bb7f221558c77154d23
Thomas Karagianes <tkaragianes@stanford.edu>
2022-07-06 12:11:15 -0700

    Update Eternacon 2022 schedule details

--- src/views/eternacon/Eternacon2022.vue
+++ src/views/eternacon/Eternacon2022.vue
@@ -23,33 +23,7 @@
         Additional questions? Contact <a href="mailto:events@eternagame.org">events@eternagame.org</a>.
       </p>
     </section>
-    <section class="schedule">
-      <h3>Speakers</h3>
-      <p>Full schedule will be released in July</p>
-      <h4>Saturday</h4>
-      <ul>
-        <li>Hannah Wayment-Steele, Harvard, OpenVaccine summary</li>
-        <li>David Resnik, NIH, Ethics in Citizen Science</li>
-        <li>whbob/amybarish, Player tools</li>
-        <li>Antje Kruger, Northwestern, OpenRibosome</li>
-        <li>Karmen Williams, CUNY, Diversity panel with player Akira</li>
-        <li>lroppy, Eterna Players Alliance</li>
-        <li>Astromon, Eternacon puzzle</li>
-      </ul>
-      <h4>Sunday</h4>
-      <ul>
-        <li>Sarah Smaga, UC-Berkeley, C-GEM and Eterna3D</li>
-        <li>Slava Butkovich, Chimera tutorial</li>
-        <li>Rachael Kretsch, Stanford, Tertiary structures</li>
-        <li>Janet Iwasa, University of Utah, Animating Molecular Machines</li>
-        <li>Eterna developers, EternaNext</li>
-        <li>Jennifer Pearl, Pairing probabilities</li>
-        <li>Astromon, Art show</li>
-        <li>AndrewKae, Paste bin fun</li>
-      </ul>
-      <h4>Want to present?</h4>
-      <p>Get in touch at <a href="mailto:events@eternagame.org">events@eternagame.org</a></p>
-    </section>
+    <ScheduleSection :sessions="sessions" />
     <template #sidebar="{ isInSidebar }">
       <DropdownSidebarPanel
         :options="options"
@@ -70,11 +44,16 @@
   import ConferenceAreasSection from './components/ConferenceAreasSection.vue';
   import ScheduleSection from './components/ScheduleSection.vue';
   import { options } from './AboutEternacon.vue';
+  import sessions from './speakers-2022.json';
 
   @Component({
     components: { EternaPage, HeroSection, ConferenceAreasSection, ScheduleSection, DropdownSidebarPanel },
   })
-  export default class Eternacon2020 extends Vue {
+  export default class Eternacon2022 extends Vue {
+    get sessions() {
+      return sessions;
+    }
+
     get options() {
       return options;
     };
